47d3b86c0cb632207101346fc2cdf898dc0909ec
darinoos47 <sepehr.eskandari@gmail.com>
2022-04-03 20:38:15 -0700

    TMz Odd mode has been added.

--- FDTD_Mode_Excitation_SEPEHR.m
+++ FDTD_Mode_Excitation_SEPEHR.m
@@ -135,45 +135,25 @@ for n=1:nTotal
    Hy = Hy + Hy1.*By - Hy2.*By_BeforeUpdate; % step 4
    Dz(2:Ny,2:Nx) = Dz1(2:Ny,2:Nx).*Dz(2:Ny,2:Nx) + dt./Dz2(2:Ny,2:Nx)./er(2:Ny,2:Nx).*((Hy(2:Ny,2:end)-Hy(2:Ny,1:(Nx-1)))/dx-(Hx(2:end,2:Nx)-Hx(1:(Ny-1),2:Nx))/dy); % step 5
    Ez(2:Ny,2:Nx) = Ez1(2:Ny,2:Nx).*Ez(2:Ny,2:Nx) + 1./Ez2(2:Ny,2:Nx).*(Dz(2:Ny,2:Nx)-Dz_BeforeUpdate(2:Ny,2:Nx)); % step 6
-   Ez(127:137,L+10) = exp(-0.8936*2.2)/sin(1.0308*2.2)*sin(1.0308e7*(1:11)*dy)*cos(w0*n*dt);
-   Ez(126,L+10) = 0;
-   Ez(115:125,L+10) = -flipud(Ez(127:137,L+10));
-   Ez(138:220,L+10) = exp(-0.8936*1e7*dy*(12:94));
-   Ez(32:114,L+10) = -flipud(Ez(138:220,L+10));
-%    Ez()
-%    J(n) = exp(-((n-1/2)*dt-4*sigma)^2/(sigma)^2)*sin(w0*((n-1/2)*dt-4*sigma));
-%    Ez(row_source,col_source) = Ez(row_source,col_source) - dt/er(row_source,col_source)*J(n);
-%    Ez
+   %%%%% Fundamental Odd TM Mode
+%    Ez(127:137,L+10) = exp(-0.8936*2.2)/sin(1.0308*2.2)*sin(1.0308e7*(1:11)*dy)*cos(w0*n*dt);
+%    Ez(126,L+10) = 0;
+%    Ez(115:125,L+10) = -flipud(Ez(127:137,L+10));
+%    Ez(138:220,L+10) = exp(-0.8936*1e7*dy*(12:94))*cos(w0*n*dt);
+%    Ez(32:114,L+10) = -flipud(Ez(138:220,L+10));
+   %%%%% Fundamental Even TM Mode
+   Ez(127:137,L+10) = exp(-1.236*2.2)/cos(0.5299*2.2)*cos(5.299e6*(1:11)*dy)*cos(w0*n*dt);
+   Ez(126,L+10) = exp(-1.236*2.2)/cos(0.5299*2.2)*cos(w0*n*dt);
+   Ez(115:125,L+10) = flipud(Ez(127:137,L+10));
+   Ez(138:220,L+10) = exp(-1.236*1e7*dy*(12:94))*cos(w0*n*dt);
+   Ez(32:114,L+10) = flipud(Ez(138:220,L+10));
+   %%%%%
    Ez_Probe(n) = Ez(row_observation,col_observation);
-%    if(n>6000)
-%    pcolor(X,Y,Ez);
-%    title(['Electric Field Profile at ', num2str(n), 'th time-step'])
-%    colorbar
-%    rectangle('Position',[-2500e-9,-220e-9,5000-9,440e-9])
-%    caxis([-0.2 0.2])
-%    shading interp
-%    pause(0.01);
-%    end
-% n
-   if(n==2e3)
-       Ez_2DFigure = Ez;
-   end
+   pcolor(X,Y,Ez);
+   title(['Electric Field Profile at ', num2str(n), 'th time-step'])
+   colorbar
+   rectangle('Position',[-2500e-9,-220e-9,5000-9,440e-9])
+   caxis([-0.2 0.2])
+   shading interp
+   pause(0.01);
 end
-% figure
-% pcolor(X*1e9,Y*1e9,Ez_2DFigure);
-% % rectangle('Position',[-1500,-90,3000,180])
-% rectangle('Position',[-2500e-9,-220e-9,1300e-9,440e-9])
-% caxis([-4e-10 4e-10])
-% xlabel('X [nm]')
-% ylabel('Y [nm]')
-% title('Electric Field Profile at The 1500th Time-step')
-% shading interp
-% colorbar
-% rectangle('Position',[-2500,-220,1300,440])
-% figure
-% set(gcf,'position',[100,100,1500,550])
-% plot(log10(abs(Ez_Probe)),'LineWidth',1.5)
-% xlabel('Time step','FontSize',12)
-% ylabel('log_{10}(|E_{z}|)','FontSize',12)
-% title('Logarithm of the Magnitude of the Recorded Electric Field at the Proble Location (x=1000nm,y=0)','FontSize',15)
-% grid
