1b56361b9e490a3960bd984be2165d8446f3b188
darinoos47 <sepehr.eskandari@gmail.com>
2022-04-06 12:53:46 -0700

    A figure of merit has been added to the code.

--- FDTD_TM_SEPEHR.m
+++ FDTD_TM_SEPEHR.m
@@ -4,7 +4,7 @@
 % Date: 3/16/2022
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 close all;
-clear all;
+clearvars;
 clc;
 
 % Domain Size
@@ -16,18 +16,16 @@ dy = 20e-9;
 % Time step
 dt = dx/sqrt(2);
 % Total number of time steps
-nTotal = 1e4;
+nTotal = 10e3;
 % Number of grids in each direction
 Nx = floor(xDim/dx);
 Ny = floor(yDim/dy);
 % Inhomogeneity
-
 er = ones(Ny+1,Nx+1);
+er(66:187,66:187) = 12;
 er(151:173,186:end) = 12;
 er(79:101,186:end) = 12;
-er(115:137,1:65) = 12;
-% er(66:186,66:186) = double(12*(rand(121,121) < 0.5));
-er(66:186,66:186) = 12;
+er(115:137,:) = 12;
 % Initialization of fields
 Ez = zeros(Ny+1,Nx+1);
 Dz = zeros(Ny+1,Nx+1);
@@ -42,14 +40,14 @@ lambdaU = 1566e-9;
 w0 = 2*pi/lambda0;
 sigma = 2*lambda0/w0/(lambdaU-lambdaL);
 % Source and probe locations
-xSource = -1850e-9;
-ySource = 0;
-x_observation = 1000e-9;
-y_observation = 0;
+%xSource = -1850e-9;
+%ySource = 0;
+x_observation = 2020e-9;
+y_observation = 500e-9;
 [X,Y] = meshgrid(linspace(-xDim/2,xDim/2,Nx+1),linspace(-yDim/2,yDim/2,Ny+1));
-diff_source = sqrt((X-xSource).^2+(Y-ySource).^2);
-[~,idx]=min(diff_source(:));
-[row_source,col_source]=ind2sub(size(diff_source),idx);
+%diff_source = sqrt((X-xSource).^2+(Y-ySource).^2);
+%[~,idx]=min(diff_source(:));
+%[row_source,col_source]=ind2sub(size(diff_source),idx);
 diff_observation = sqrt((X-x_observation).^2+(Y-y_observation).^2);
 [~,idx]=min(diff_observation(:));
 [row_observation,col_observation]=ind2sub(size(diff_observation),idx);
@@ -125,11 +123,15 @@ for i=1:L
 end
 Ez1 = (1-Ez_SigmaY*dt/2)./(1+Ez_SigmaY*dt/2);
 Ez2 = 1+Ez_SigmaY*dt/2;
-
+% For defining a figure of merit, I recored electric field at
+% (2020nm,500nm) as the observation point and at (0,480nm) as excitation 
+% point and then calculated the ratio of maximum of probe location field to
+% the excitation location field.
 Ez_Probe = zeros(nTotal,1);
+Ez_Excitation = zeros(nTotal,1);
 Ez_2DFigure = zeros(Ny+1,Nx+1);
 % Update loop
-J = zeros(nTotal,1);
+%J = zeros(nTotal,1);
 for n=1:nTotal
    Bx_BeforeUpdate = Bx;
    By_BeforeUpdate = By;
@@ -140,41 +142,43 @@ for n=1:nTotal
    Hy = Hy + Hy1.*By - Hy2.*By_BeforeUpdate; % step 4
    Dz(2:Ny,2:Nx) = Dz1(2:Ny,2:Nx).*Dz(2:Ny,2:Nx) + dt./Dz2(2:Ny,2:Nx)./er(2:Ny,2:Nx).*((Hy(2:Ny,2:end)-Hy(2:Ny,1:(Nx-1)))/dx-(Hx(2:end,2:Nx)-Hx(1:(Ny-1),2:Nx))/dy); % step 5
    Ez(2:Ny,2:Nx) = Ez1(2:Ny,2:Nx).*Ez(2:Ny,2:Nx) + 1./Ez2(2:Ny,2:Nx).*(Dz(2:Ny,2:Nx)-Dz_BeforeUpdate(2:Ny,2:Nx)); % step 6
-   J(n) = exp(-((n-1/2)*dt-4*sigma)^2/(sigma)^2)*sin(w0*((n-1/2)*dt-4*sigma));
-   Ez(row_source,col_source) = Ez(row_source,col_source) - dt/er(row_source,col_source)*J(n);
+   %%%%% Fundamental Odd TM Mode
+%    Ez(127:137,L+10) = exp(-0.8936*2.2)/sin(1.0308*2.2)*sin(1.0308e7*(1:11)*dy)*cos(w0*n*dt);
+%    Ez(126,L+10) = 0;
+%    Ez(115:125,L+10) = -flipud(Ez(127:137,L+10));
+%    Ez(138:220,L+10) = exp(-0.8936*1e7*dy*(12:94))*cos(w0*n*dt);
+   Ez(32:114,L+10) = -flipud(Ez(138:220,L+10));
+   %%%%% Fundamental Even TM Mode
+   % Right Now the sources are hard. Need to be modified later.
+   Ez(127:137,L+10) = (exp(-1.236*2.2)/cos(0.5299*2.2)*cos(5.299e6*(1:11)*dy)*exp(-(n*dt-4*sigma)^2/(sigma^2))*sin(w0*(n*dt-4*sigma)))';
+   Ez(126,L+10) = (exp(-1.236*2.2)/cos(0.5299*2.2)*exp(-(n*dt-4*sigma)^2/(sigma^2))*sin(w0*(n*dt-4*sigma)))';
+   Ez(115:125,L+10) = flipud(Ez(127:137,L+10));
+   Ez(138:220,L+10) = (exp(-1.236*1e7*dy*(12:94))*exp(-(n*dt-4*sigma)^2/(sigma^2))*sin(w0*(n*dt-4*sigma)))';
+   Ez(32:114,L+10) = flipud(Ez(138:220,L+10));
+
+%    %%%%% Fundamental Even TM Mode
+%    Ez(127:137,L+10) = Ez(127:137,L+10) + (exp(-1.236*2.2)/cos(0.5299*2.2)*cos(5.299e6*(1:11)*dy)*exp(-(n*dt-4*sigma)^2/(sigma^2))*sin(w0*(n*dt-4*sigma)))';
+%    Ez(126,L+10) = Ez(126,L+10) + (exp(-1.236*2.2)/cos(0.5299*2.2)*exp(-(n*dt-4*sigma)^2/(sigma^2))*sin(w0*(n*dt-4*sigma)))';
+%    Ez(115:125,L+10) = flipud(Ez(127:137,L+10));
+%    Ez(138:220,L+10) = Ez(138:220,L+10) + (exp(-1.236*1e7*dy*(12:94))*exp(-(n*dt-4*sigma)^2/(sigma^2))*sin(w0*(n*dt-4*sigma)))';
+%    Ez(32:114,L+10) = flipud(Ez(138:220,L+10));
+%     
+
+   %%%%%
    Ez_Probe(n) = Ez(row_observation,col_observation);
-   if(n>3000)
-   pcolor(X,Y,Ez);
-   title(['Electric Field Profile at ', num2str(n), 'th time-step'])
-   colorbar
-   rectangle('Position',[-2500e-9,-220e-9,1300e-9,440e-9])
-   rectangle('Position',[-1200e-9,-1200e-9,2400e-9,2400e-9])
-   rectangle('Position',[1200e-9,-720e-9,1300e-9,440e-9])
-   rectangle('Position',[1200e-9,280e-9,1300e-9,440e-9])
-   caxis([-3e-10 3e-10])
-   shading interp
-   pause(0.01);
-   end
-% n
-   if(n==1.5e3)
-       Ez_2DFigure = Ez;
-   end
+   Ez_Excitation(n) = Ez(126,L+10);
+%    if(n>4000)
+%        pcolor(X,Y,Ez);
+%        title(['Electric Field Profile at ', num2str(n), 'th time-step'])
+%        colorbar
+%        rectangle('Position',[-1200e-9,-1200e-9,2400e-9,2400e-9]);
+%        rectangle('Position',[-2500e-9,-220e-9,1300e-9,440e-9]);
+%        rectangle('Position',[1200e-9,-720e-9,1300e-9,440e-9]);
+%        rectangle('Position',[1200e-9,280e-9,1300e-9,440e-9]);
+%        caxis([-0.1 0.1])
+%        shading interp
+%        pause(0.01);
+%    end
+n
 end
-% figure
-% pcolor(X*1e9,Y*1e9,Ez_2DFigure);
-% % rectangle('Position',[-1500,-90,3000,180])
-% rectangle('Position',[-2500e-9,-220e-9,1300e-9,440e-9])
-% caxis([-4e-10 4e-10])
-% xlabel('X [nm]')
-% ylabel('Y [nm]')
-% title('Electric Field Profile at The 1500th Time-step')
-% shading interp
-% colorbar
-% rectangle('Position',[-2500,-220,1300,440])
-% figure
-% set(gcf,'position',[100,100,1500,550])
-% plot(log10(abs(Ez_Probe)),'LineWidth',1.5)
-% xlabel('Time step','FontSize',12)
-% ylabel('log_{10}(|E_{z}|)','FontSize',12)
-% title('Logarithm of the Magnitude of the Recorded Electric Field at the Proble Location (x=1000nm,y=0)','FontSize',15)
-% grid
+efficiency = max(Ez_Probe) / max(Ez_Excitation);
