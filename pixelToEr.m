665ac0f15a28daf7837429f560ea14c1a20799b6
piriln <pirilnergis@gmail.com>
2022-04-09 10:43:41 -0700

    Update pixelToEr.m

--- pixelToEr.m
+++ pixelToEr.m
@@ -12,11 +12,14 @@ function er = pixelToEr(pbs, er, erSi, Ny, Nx, dy, dx, pbs_size)
     btmleftcornerX = origin(2)-pbs_size/2/dx;
     pixEr = pix_size/dx;
     for i=1:Npix
+        p1 = btmleftcornerY;
         for j=1:Npix
+            p2 = floor(btmleftcornerY+(j)*pixEr)-1;
             if pbs(i,j) == 1
-                er(floor(btmleftcornerY+(j-1)*pixEr):floor(btmleftcornerY+(j)*pixEr),...
+                er(p1:p2,...
                    floor(btmleftcornerX+(i-1)*pixEr):floor(btmleftcornerX+(i)*pixEr)-1) = erSi;
             end
+            p1=p2+1;
         end
     end
 end
