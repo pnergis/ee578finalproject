8b5399ab2ae8cba02f68c1c23ac0c25c6643b091
Sepehr Eskandari <31010518+darinoos47@users.noreply.github.com>
2022-04-21 13:25:20 -0700

    TE and TM performance vectors are implemented.

--- Genetic Algorithm/sorterGA.m
+++ Genetic Algorithm/sorterGA.m
@@ -1,4 +1,4 @@
-function [populationMatrix,performanceVector] = sorterGA(populationMatrix,performanceVector)
+function [populationMatrix,performanceVector,performanceVectorTM,performanceVectorTE] = sorterGA(populationMatrix,performanceVector,performanceVectorTM,performanceVectorTE);
 %Summary of this function goes here
 %   populationMatrix is a popSize by numofParam matrix, where popSize is
 %   the population size in genetic algorithm, and numofParam is the number
@@ -7,5 +7,7 @@ function [populationMatrix,performanceVector] = sorterGA(populationMatrix,perfor
 %   Our goal here is to sort the rows of populationMatrix in a descending
 %   order according to their correspoding performance in performanceVector.
 [performanceVector,I] = sort(performanceVector,'descend');
+performanceVectorTM = performanceVectorTM(I);
+performanceVectorTE = performanceVectorTE(I);
 populationMatrix = populationMatrix(I,:);
 end
