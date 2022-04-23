9670c51b108f1998047e96ce15b6c1d0216f8c9c
Sepehr Eskandari <31010518+darinoos47@users.noreply.github.com>
2022-04-23 14:29:43 -0700

    Recording Performance
    A matrix to save the performance over iterations has been added.

--- Genetic Algorithm/GA.m
+++ Genetic Algorithm/GA.m
@@ -1,11 +1,12 @@
 % Dont forget to comment s
 popSize = 20; % Size of the population, must be a multiply of 4.
-numberofIterations = 30;
+numberofIterations = 100;
 mutationProbability = 0.1; % Mutation probability.
 performanceVector = zeros(popSize,1); % A vector of size popSize.
 performanceVectorTM = zeros(popSize,1);
 performanceVectorTE = zeros(popSize,1);
 populationMatrix = randi(2,[popSize,400])-1; % A matrix of size popSize by numberofParams (here 400). Each row corresponds to a specific gene.
+performanceRecordedOverIteration = zeros(numberofIterations,3);
 for i=1:numberofIterations
     tic;
     % The first thing to do is to evaluate the performance of our
@@ -23,12 +24,12 @@ for i=1:numberofIterations
             % Now we use the generated profile for the simulation. Our
             % FDTD_TM has the ability to run an animation. Here we passed a
             % 0 as the second argument to disable this ability.
-            performanceVectorTE(j) = FDTD_TE(permittivity,0,0,1);
+            performanceVectorTE(j) = FDTD_TE(permittivity,0,0,0);
             performanceVectorTM(j) = FDTD_TM(permittivity,0,0,0);
-            if(performanceVectorTE(j) > 1.5*performanceVectorTM(j) || 1.5*performanceVectorTE(j) < performanceVectorTM(j))
-                performanceVectorTE(j) = 0.7*performanceVectorTE(j);
-                performanceVectorTM(j) = 0.7*performanceVectorTM(j);
-            end
+%             if(performanceVectorTE(j) > 1.5*performanceVectorTM(j) || 1.5*performanceVectorTE(j) < performanceVectorTM(j))
+%                 performanceVectorTE(j) = 0.7*performanceVectorTE(j);
+%                 performanceVectorTM(j) = 0.7*performanceVectorTM(j);
+%             end
             performanceVector(j) = (performanceVectorTE(j) + performanceVectorTM(j))/2; 
 
         end
@@ -41,8 +42,12 @@ for i=1:numberofIterations
     % Now we have a sorted generation. We need to perform crossover and
     % mutation to generate the next generation, and then repeat the for
     % loop.
+    performanceRecordedOverIteration(i,1) = performanceVector(1);
+    performanceRecordedOverIteration(i,2) = performanceVectorTM(1);
+    performanceRecordedOverIteration(i,3) = performanceVectorTE(1);
     [populationMatrix,performanceVector] = crossoverAndMutation(populationMatrix,performanceVector,mutationProbability);
     t2 = toc;
     disp(['Progress: ',num2str(100*i/numberofIterations,'%.2f'),'%'])
     disp(['Time remaining: ', num2str((numberofIterations-i)*t2/60,'%.2f'), ' minutes'])
 end
+save('GA_DATA_TEeven_TMeven.mat')
