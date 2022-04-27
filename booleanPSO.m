6ebbc65774814e8f91dfb10c705c4a5b179fefd3
Sepehr Eskandari <31010518+darinoos47@users.noreply.github.com>
2022-04-27 09:27:37 -0700

    Change the random c1 and c2 be generated for each particle.

--- booleanPSO.m
+++ booleanPSO.m
@@ -1,18 +1,21 @@
-% Dont forget to comment s
-popSize = 10; % Size of the population, must be a multiply of 4.
+popSize = 10; 
 numberofIterations = 10;
+swarm = true([popSize,400]);
+for i=1:popSize
+    for j=1:5
+        swarm(i,randi(400)) = false;
+    end
+end
 
-swarm = logical(randi(2,[popSize,400])-1); % A matrix of size popSize by numberofParams (here 400). Each row corresponds to a specific particle.
 velocitySwarm = false([popSize,400]);
 performanceVectorCurrent = zeros(popSize,1); % A vector of size popSize used to store the performence of particle in each iteration.
 pBest = false([popSize,400]); % A matrix of size popSize by numberofParams (here 400). ith row corresponds to the best position ith particle has been ever explored.
 gBest = false([1,400]); % A vector of size 1 by numberofParams. The best overall point ever explored (Best of pBests).
 performanceVectorPBest = zeros(popSize,1);
 performanceGBest = 0;
-performanceVectorTM = zeros(popSize,1);
-performanceVectorTE = zeros(popSize,1);
 
-for i=1:numberofIterations
+% for i=1:numberofIterations
+while(performanceGBest<0.85)
     tic;
     % The first thing to do is to evaluate the performance of our swarm
     for j=1:popSize
@@ -25,9 +28,8 @@ for i=1:numberofIterations
         % Now we use the generated profile for the simulation. Our
         % FDTD_TM has the ability to run an animation. Here we passed a
         % 0 as the second argument to disable this ability.
-        performanceVectorTE(j) = FDTD_TE(permittivity,0,0,0);
-        performanceVectorTM(j) = FDTD_TM(permittivity,0,0,0);
-        performanceVectorCurrent(j) = (performanceVectorTE(j) + performanceVectorTM(j))/2;
+
+        performanceVectorCurrent(j) = FDTD_TM(permittivity,0,0,0);
         if(performanceVectorCurrent(j) > performanceVectorPBest(j))
             performanceVectorPBest(j) = performanceVectorCurrent(j);
             pBest(j,:) = swarm(j,:);
@@ -37,48 +39,30 @@ for i=1:numberofIterations
             gBest = swarm(j,:);
         end
     end
-    if(rand < 0.5)
-        c1 = true;
-    else
-        c1 = false;
-    end
-    if(rand < 0.5)
-        c2 = true;
-    else
-        c2 = false;
-    end
-    if(rand < 0.1)
-        w = true;
-    else
-        w = false;
-    end
-    velocitySwarm = (w&velocitySwarm) | (c1&xor(pBest,swarm(j,:))) | (c1&xor(gBest,swarm(j,:)));
     % In case that c1 and c2 need to be generated for each particle
     % separately.
     % Now we want to calculate the velocities and update our particles.
     % c1 and c2 are set to be 2.
-    %     for j=1:popSize
-    %         if(rand < 0.5)
-    %             c1 = true;
-    %         else
-    %             c1 = false;
-    %         end
-    %         if(rand < 0.5)
-    %             c2 = true;
-    %         else
-    %             c2 = false;
-    %         end
-    %         if(rand < 0.1)
-    %             w = true;
-    %         else
-    %             w = false;
-    %         end
-    %         velocitySwarm(j,:) = (w&velocitySwarm(j,:)) | (c1&xor(pBest(j,:),swarm(j,:))) | (c1&xor(gBest,swarm(j,:)));
-    %     end
+    for j=1:popSize
+        if(rand < 0.5)
+            c1 = true;
+        else
+            c1 = false;
+        end
+        if(rand < 0.5)
+            c2 = true;
+        else
+            c2 = false;
+        end
+        if(rand < 0.1)
+            w = true;
+        else
+            w = false;
+        end
+        velocitySwarm(j,:) = (w&velocitySwarm(j,:)) | (c1&xor(pBest(j,:),swarm(j,:))) | (c1&xor(gBest,swarm(j,:)));
+    end
     swarm = xor(swarm,velocitySwarm);
     performanceVectorPBest
     performanceGBest
     t2 = toc;
-    disp(['Progress: ',num2str(100*i/numberofIterations,'%.2f'),'%'])
-    disp(['Time remaining: ', num2str((numberofIterations-i)*t2/60,'%.2f'), ' minutes'])
 end
